280ce2dae3c23311c757c2d194fc37e1b5a5a192
wadecr7 <149808957+wadecr7@users.noreply.github.com>
2024-01-19 16:44:50 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+let gameopen = document.querySelector(".parent")
+let gamebutton = document.querySelector(".mainbutton")
+
+let rock = document.querySelector ("#js-click")
+let paper = document.querySelector ("#js-click2")
+let scissors = document.querySelector ("#js-click3")
+
+let numberhuman = document.querySelector("#numberhuman")
+let numbercomputer = document.querySelector("#numbercomputer")
+
+let rockhuman = document.querySelector(".rockhuman")
+let paperhuman = document.querySelector(".paperhuman")
+let scissorshuman = document.querySelector(".scissorshuman")
+
+  let rockcomputer = document.querySelector(".rockcomputer")
+  let papercomputer = document.querySelector(".papercomputer")
+  let scissorscomputer = document.querySelector(".scissorscomputer")
+
+  gamebutton.addEventListener("click", () => {
+      gameopen.style.display = "block";
+     gamebutton.classList = "none"
+  }
+
+  ) 
+
+  
+
+  const ramdomComp = () => {
+     let game = [rockcomputer,papercomputer,scissorscomputer]
+      let randomNum = Math.floor(Math.random() * game.length);
+     let randomWord = game[randomNum]
+         for (let i = 0; i < 3;++i) {
+          if (i != randomNum ) {
+          game[i].style.display = "none";
+         }
+       }
+        randomWord.style.display = "block";
+        return randomNum;
+  }
+
+
+  
+
+
+rock.addEventListener("click",() => {
+       paperhuman.style.display = "none";
+       scissorshuman.style.display = "none";
+        rockhuman.style.display = "block" ;
+         let compchoise =   ramdomComp();
+
+
+     
+
+      
+          switch (compchoise ) {
+          case 1:
+          numbercomputer.textContent = + numbercomputer.textContent + 1;
+          break;
+          case 2:
+          numberhuman.textContent = +numberhuman.textContent + 1;
+          break;
+       }
+
+      
+})
+
+
+paper.addEventListener("click",() => {
+   
+       scissorshuman.style.display = "none";
+       rockhuman.style.display = "none";
+       paperhuman.style.display = "block";
+       let compchoise =   ramdomComp();
+        switch (compchoise) {
+          case 0:
+          numberhuman.textContent =+numberhuman.textContent +1;
+          break;
+          case 2:
+          numbercomputer.textContent =+numbercomputer.textContent +1;
+          break;
+       }
+})
+
+scissors.addEventListener("click",() => {
+    paperhuman.style.display = "none";
+    rockhuman.style.display = "none";
+    scissorshuman.style.display = "block";
+     let compchoise =   ramdomComp();
+      switch (compchoise) {
+          case 1:
+          numberhuman.textContent = +numberhuman.textContent+1;
+          break;
+          case 0:
+          numbercomputer.textContent = +numbercomputer.textContent+1;
+          break;
+       }
+    
+})
+
+
+
+
